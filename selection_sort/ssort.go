15b8ede243e4a62adc561eee02ad36ad1c607183
ralvescosta <rafael.rac.mg@gmail.com>
2024-03-15 09:35:15 -0300

    feat: selection sort

---
+++ selection_sort/ssort.go
@@ -0,0 +1,25 @@
+package ssort
+
+func SelectionSort(arr []int) []int {
+	var smallest int
+	var smallestIndex int
+
+	newArr := []int{}
+
+	for range arr {
+		smallest = arr[0]
+		smallestIndex = 0
+
+		for j := range arr {
+			if arr[j] < smallest {
+				smallest = arr[j]
+				smallestIndex = j
+			}
+		}
+
+		newArr = append(newArr, smallest)
+		println(smallestIndex)
+	}
+
+	return arr
+}
