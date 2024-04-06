3942f48ab254bfbc13beef2fdf23937edaf916b5
ralvescosta <rafael.rac.mg@gmail.com>
2024-04-06 09:15:03 -0300

    feat: quick sort

--- quick_sort/qsort.go
+++ quick_sort/qsort.go
@@ -1,11 +1,20 @@
 package quicksort
 
+import (
+	"math/rand"
+	"time"
+)
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func QuickSort(array []int) []int {
 	if len(array) < 2 {
 		return array
 	}
 
-	pivot := array[0]
+	pivotIndex := r.Intn(len(array))
+	pivot := array[pivotIndex]
+
 	less := make([]int, 0)
 	greater := []int{}
 
